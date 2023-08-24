3c5d3cc5244cd35577ecbcd9ba6a08fcfe238c86
Sajol Das <sajoldas421@example.com>
2023-08-24 14:21:44 +0600

    first

--- food/src/components/Cart/Cart.js
+++ food/src/components/Cart/Cart.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import classes from './Cart.module.css'
+import Modal from '../UI/Modal'
 const Cart = () => {
   const cartItems = (
     <ul className={classes['cart-items']}>
@@ -7,15 +8,15 @@ const Cart = () => {
     </ul>
   )
   return (
-    <div>
-      <cartItems />
+    <Modal>
+      <cartItems/>
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <button className={classes['button--alt']}>close</button>
       <button className={classes.button}>Order</button>
-    </div>
+    </Modal>
   )
 }
 
